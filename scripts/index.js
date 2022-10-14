af6a884d48b286a4cc99f6a8fb2d2ed2749d4987
nandita <nanditavora@yahoo.co.in>
2022-10-14 15:29:05 +0530

    fetched all groups

--- scripts/index.js
+++ scripts/index.js
@@ -16,6 +16,7 @@ function fetchMembers() {
                 }
             })
                 .then(response => {
+                    console.log(response.headers["x-total-pages"]);
                     const groups = response.data.filter(group => group.full_path == userInput);
                     if (groups.length > 0) {
                         const groupName = groups[0].full_name;
@@ -26,7 +27,7 @@ function fetchMembers() {
                             }
                         })
                             .then(response => {
-                                console.log(response.data);
+                                // console.log(response.data);
                                 document.getElementById("total-members").innerHTML = `Total Members in the <u>${groupName}</u> group are <u>${response.data.length}</u>`;
                                 document.getElementById("member-list").innerHTML = response.data.map(member => `<li>${member.id} - ${member.name} - ${member.username}</li>`).join("");
                             })
@@ -55,38 +56,52 @@ function fetchSubgroups() {
 
     const URL = "https://myrepos.stackroute.niit.com/api/v4/groups";
     const PRIVATE_TOKEN = prompt("Enter Git Access Token");
-
-    axios.get(`${URL}?per_page=200`, {
-        headers: {
-            "PRIVATE-TOKEN": PRIVATE_TOKEN
-        }
-    })
-        .then(response => {
-            document.getElementById("member-data").style.display = "none";
-            document.getElementById("subGroup-data").style.display = "block";
-            const userInput = document.getElementById("group-name-input").value.trim();
-            console.log(response.data.filter(group => group.full_path.startsWith(userInput)));
-            if (userInput !== "") {
-                const groups = response.data.filter(group => group.full_path.startsWith(userInput) && group.full_path !== userInput);
-                console.log(response.data)
-                if (groups.length > 0) {
-                    document.getElementById("total-subGroups").innerHTML = `Total Sub Groups in the <u>${userInput}</u> group are <u>${groups.length}</u>`;
-                    document.getElementById("subGroup-list").innerHTML = groups.map(subGroup => `<li>${subGroup.full_path}</li>`).join("");
-
-                }
-                else {
-                    document.getElementById("total-subGroups").innerHTML = `No Group with the inputted Group Name Exists !!!`;
-                    document.getElementById("subGroup-list").innerHTML = "";
-                }
-            }
-            else {
-                document.getElementById("total-subGroups").innerHTML = `Please Provide Group Name`;
-                document.getElementById("subGroup-list").innerHTML = "";
+    const userInput = document.getElementById("group-name-input").value.trim();
+    if (userInput !== "") {
+        axios.get(`${URL}?per_page=100`, {
+            headers: {
+                "PRIVATE-TOKEN": PRIVATE_TOKEN
             }
         })
+            .then(response => {
 
-        .catch(error => {
-            console.log(error);
-        })
+                document.getElementById("member-data").style.display = "none";
+                document.getElementById("subGroup-data").style.display = "block";
+                const responseData = response.data;
+
+                for (let i = 2; i <= response.headers["x-total-pages"]; i++) {
+                    axios.get(`${URL}?per_page=100&page=${i}`, {
+                        headers: {
+                            "PRIVATE-TOKEN": PRIVATE_TOKEN
+                        }
+                    })
+                        .then(response => {
+                            responseData.push(...response.data);
+                            console.log(responseData);
+                            const groups = responseData.filter(group => group.full_path.startsWith(userInput) && group.full_path !== userInput);
+                            if (groups.length > 0) {
+                                document.getElementById("total-subGroups").innerHTML = `Total Sub Groups in the <u>${userInput}</u> group are <u>${groups.length}</u>`;
+                                document.getElementById("subGroup-list").innerHTML = groups.map(subGroup => `<li>${subGroup.full_path}</li>`).join("");
+
+                            }
+                            else {
+                                document.getElementById("total-subGroups").innerHTML = `No Group with the inputted Group Name Exists !!!`;
+                                document.getElementById("subGroup-list").innerHTML = "";
+                            }
+                        })
+                }
+
+            })
+
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    else {
+        document.getElementById("total-subGroups").innerHTML = `Please Provide Group Name`;
+        document.getElementById("subGroup-list").innerHTML = "";
+    }
+
+
 
 }

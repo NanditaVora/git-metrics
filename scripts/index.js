a83c600eda7ba07547f0017c7bda52a07f102c05
nandita <nanditavora@yahoo.co.in>
2022-10-14 11:55:16 +0530

    updated code to fetch token

--- scripts/index.js
+++ scripts/index.js
@@ -3,42 +3,52 @@
 function fetchMembers() {
 
     const URL = "https://myrepos.stackroute.niit.com/api/v4/groups";
-
-    const PRIVATE_TOKEN = prompt("Enter Git Access Token");
-    axios.get(`${URL}?per_page=100`, {
-        headers: {
-            "PRIVATE-TOKEN": PRIVATE_TOKEN
-        }
-    })
-        .then(response => {
-            document.getElementById("member-data").style.display = "block";
-            document.getElementById("subGroup-data").style.display = "none";
-            const userInput = document.getElementById("group-name-input").value.trim();
-            const groups = response.data.filter(group => group.full_path == userInput);
-            if (groups.length > 0) {
-                const groupName = groups[0].full_name;
-                const projectURL = `${URL}/${groups[0].id}/members`;
-                axios.get(projectURL, {
-                    headers: {
-                        "PRIVATE-TOKEN": PRIVATE_TOKEN
+    const userInput = document.getElementById("group-name-input").value.trim();
+    document.getElementById("member-data").style.display = "block";
+    document.getElementById("subGroup-data").style.display = "none";
+    if (userInput !== "") {
+        const PRIVATE_TOKEN = prompt("Enter Git Access Token", "").trim();
+        alert(PRIVATE_TOKEN.length);
+        if (PRIVATE_TOKEN.length > 0) {
+            axios.get(`${URL}?per_page=100`, {
+                headers: {
+                    "PRIVATE-TOKEN": PRIVATE_TOKEN
+                }
+            })
+                .then(response => {
+                    const groups = response.data.filter(group => group.full_path == userInput);
+                    if (groups.length > 0) {
+                        const groupName = groups[0].full_name;
+                        const projectURL = `${URL}/${groups[0].id}/members`;
+                        axios.get(projectURL, {
+                            headers: {
+                                "PRIVATE-TOKEN": PRIVATE_TOKEN
+                            }
+                        })
+                            .then(response => {
+                                console.log(response.data);
+                                document.getElementById("total-members").innerHTML = `Total Members in the <u>${groupName}</u> group are <u>${response.data.length}</u>`;
+                                document.getElementById("member-list").innerHTML = response.data.map(member => `<li>${member.id} - ${member.name} - ${member.username}</li>`).join("");
+                            })
+                    }
+                    else {
+                        document.getElementById("total-members").innerHTML = `No Group with the inputted Group Name Exists !!!`;
+                        document.getElementById("member-list").innerHTML = "";
                     }
                 })
-                    .then(response => {
-                        console.log(response.data);
-                        document.getElementById("total-members").innerHTML = `Total Members in the <u>${groupName}</u> group are <u>${response.data.length}</u>`;
-                        document.getElementById("member-list").innerHTML = response.data.map(member => `<li>${member.id} - ${member.name} - ${member.username}</li>`).join("");
-                    })
-            }
-            else {
-                document.getElementById("total-members").innerHTML = `No Group with the inputted Group Name Exists !!!`;
-                document.getElementById("member-list").innerHTML = "";
-            }
-        })
-
-        .catch(error => {
-            console.log(error.data);
-        })
-
+                .catch(error => {
+                    console.log(error.data);
+                })
+        }
+        else {
+            document.getElementById("total-members").innerHTML = `Please provide Git Access Token to proceed !!`;
+            document.getElementById("member-list").innerHTML = "";
+        }
+    }
+    else {
+        document.getElementById("total-members").innerHTML = `Please provide group name to proceed !!`;
+        document.getElementById("member-list").innerHTML = "";
+    }
 }
 
 function fetchSubgroups() {
@@ -55,9 +65,9 @@ function fetchSubgroups() {
             document.getElementById("member-data").style.display = "none";
             document.getElementById("subGroup-data").style.display = "block";
             const userInput = document.getElementById("group-name-input").value.trim();
-            console.log(response.data.filter(group=>group.full_path.startsWith(userInput)));
+            console.log(response.data.filter(group => group.full_path.startsWith(userInput)));
             if (userInput !== "") {
-                const groups = response.data.filter(group =>  group.full_path.startsWith(userInput) && group.full_path !== userInput);
+                const groups = response.data.filter(group => group.full_path.startsWith(userInput) && group.full_path !== userInput);
                 console.log(response.data)
                 if (groups.length > 0) {
                     document.getElementById("total-subGroups").innerHTML = `Total Sub Groups in the <u>${userInput}</u> group are <u>${groups.length}</u>`;
